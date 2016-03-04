0302250fbad9f3677618f88bf6e69c663637b18d
Bastien Miclo <bmiclo@novahealthsa.com>
2016-03-04 16:53:43 +0100

    Session tests

---
+++ tests/features/SessionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use CsrfProtect\CsrfProtect as Csrf;
+
+class SessionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSessionAutoStart()
+    {
+        session_destroy();
+        $this->assetTrue(!session_id());
+
+        Csrf::getTokenIndex();
+        $this->assetFalse(!session_id());
+        session_destroy();
+        $this->assetTrue(!session_id());
+
+        Csrf::getToken();
+        $this->assetFalse(!session_id());
+        session_destroy();
+        $this->assetTrue(!session_id());
+
+        Csrf::getTag();
+        $this->assetFalse(!session_id());
+        session_destroy();
+        $this->assetTrue(!session_id());
+
+        Csrf::checkToken();
+        $this->assetFalse(!session_id());
+        session_destroy();
+        $this->assetTrue(!session_id());
+
+        Csrf::checkPostToken();
+        $this->assetFalse(!session_id());
+        session_destroy();
+        $this->assetTrue(!session_id());
+
+        Csrf::isValidToken();
+        $this->assetFalse(!session_id());
+        session_destroy();
+        $this->assetTrue(!session_id());
+
+        Csrf::isValidToken();
+        $this->assetFalse(!session_id());
+        session_destroy();
+        $this->assetTrue(!session_id());
+    }
+}
