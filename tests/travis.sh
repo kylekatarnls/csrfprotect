8748fb09ed4345b66de8dc9e6dca155850af43ad
Kyle <kylekatarnls@users.noreply.github.com>
2016-03-05 12:42:19 +0100

    Sudo travis

--- tests/travis.sh
+++ tests/travis.sh
@@ -7,7 +7,7 @@ cd zephir
 cd ..
 git clone https://github.com/kylekatarnls/csrfprotect
 cd csrfprotect
-zephir build
+sudo zephir build
 cd ..
 composer install --no-interaction --prefer-source --dev
 phpenv config-add tests/conf/php-csrfprotect.ini
