48da512e0799a31ab42557044a21acff149a6a19
Kyle <kylekatarnls@gmail.com>
2016-03-05 22:20:43 +0100

    Session tests

--- tests/features/SessionTest.php
+++ tests/features/SessionTest.php
@@ -6,42 +6,46 @@ class SessionTest extends \PHPUnit_Framework_TestCase
 {
     public function testSessionAutoStart()
     {
-        session_destroy();
+        ob_start();
+
+        session_write_close();
         $this->assertTrue(!session_id());
 
         Csrf::getTokenIndex();
         $this->assertFalse(!session_id());
-        session_destroy();
+        session_write_close();
         $this->assertTrue(!session_id());
 
         Csrf::getToken();
         $this->assertFalse(!session_id());
-        session_destroy();
+        session_write_close();
         $this->assertTrue(!session_id());
 
         Csrf::getTag();
         $this->assertFalse(!session_id());
-        session_destroy();
+        session_write_close();
         $this->assertTrue(!session_id());
 
         Csrf::checkToken();
         $this->assertFalse(!session_id());
-        session_destroy();
+        session_write_close();
         $this->assertTrue(!session_id());
 
         Csrf::checkPostToken();
         $this->assertFalse(!session_id());
-        session_destroy();
+        session_write_close();
         $this->assertTrue(!session_id());
 
         Csrf::isValidToken();
         $this->assertFalse(!session_id());
-        session_destroy();
+        session_write_close();
         $this->assertTrue(!session_id());
 
         Csrf::isValidToken();
         $this->assertFalse(!session_id());
-        session_destroy();
+        session_write_close();
         $this->assertTrue(!session_id());
+
+        ob_end_clean();
     }
 }
