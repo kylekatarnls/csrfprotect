1808c6ae547315d35935bbb6cd74fda63f4f91f1
Kyle <kylekatarnls@users.noreply.github.com>
2016-03-05 16:38:31 +0100

    Use local zephir

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-mkdir .build-travis
-cd .build-travis
-git clone -b patch-1 https://github.com/kylekatarnls/zephir
-cd zephir
-./install -c
-cd ..
-git clone https://github.com/kylekatarnls/csrfprotect
-cd csrfprotect
-sudo zephir build
-cd ../..
-rm -r .build-travis
-composer install --no-interaction --prefer-source --dev
 phpenv config-add tests/conf/php-csrfprotect.ini
+composer install --no-interaction --prefer-source --dev
+cd csrfprotect
+sudo ../vendor/bin/zephir build
