afc022a1cf01397d71466022d6136c0c2fc880f9
Kyle <kylekatarnls@gmail.com>
2016-03-05 22:12:55 +0100

    Fix grammar

--- tests/features/SessionTest.php
+++ tests/features/SessionTest.php
@@ -2,51 +2,46 @@
 
 use CsrfProtect\CsrfProtect as Csrf;
 
-if (!class_exists('SessionTest', false))
-{
-    return;
-}
-
 class SessionTest extends \PHPUnit_Framework_TestCase
 {
     public function testSessionAutoStart()
     {
         session_destroy();
-        $this->assetTrue(!session_id());
+        $this->assertTrue(!session_id());
 
         Csrf::getTokenIndex();
-        $this->assetFalse(!session_id());
+        $this->assertFalse(!session_id());
         session_destroy();
-        $this->assetTrue(!session_id());
+        $this->assertTrue(!session_id());
 
         Csrf::getToken();
-        $this->assetFalse(!session_id());
+        $this->assertFalse(!session_id());
         session_destroy();
-        $this->assetTrue(!session_id());
+        $this->assertTrue(!session_id());
 
         Csrf::getTag();
-        $this->assetFalse(!session_id());
+        $this->assertFalse(!session_id());
         session_destroy();
-        $this->assetTrue(!session_id());
+        $this->assertTrue(!session_id());
 
         Csrf::checkToken();
-        $this->assetFalse(!session_id());
+        $this->assertFalse(!session_id());
         session_destroy();
-        $this->assetTrue(!session_id());
+        $this->assertTrue(!session_id());
 
         Csrf::checkPostToken();
-        $this->assetFalse(!session_id());
+        $this->assertFalse(!session_id());
         session_destroy();
-        $this->assetTrue(!session_id());
+        $this->assertTrue(!session_id());
 
         Csrf::isValidToken();
-        $this->assetFalse(!session_id());
+        $this->assertFalse(!session_id());
         session_destroy();
-        $this->assetTrue(!session_id());
+        $this->assertTrue(!session_id());
 
         Csrf::isValidToken();
-        $this->assetFalse(!session_id());
+        $this->assertFalse(!session_id());
         session_destroy();
-        $this->assetTrue(!session_id());
+        $this->assertTrue(!session_id());
     }
 }
