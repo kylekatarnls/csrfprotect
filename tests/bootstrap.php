027762b7d525d7e49d1d5cd47f36b9b86709777d
Bastien <kylekatarnls@gmail.com>
2016-03-06 13:35:08 +0100

    Session checks

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,50 +9,52 @@ $_SESSION = array();
 $trues = array();
 $falses = array();
 
-session_write_close();
+session_destroy();
 $trues[] = !session_id();
 
 Csrf::getTokenIndex();
 $falses[] = !session_id();
-session_write_close();
+session_destroy();
 $trues[] = !session_id();
 
 Csrf::getToken();
 $falses[] = !session_id();
-session_write_close();
+session_destroy();
 $trues[] = !session_id();
 
 Csrf::getTag();
 $falses[] = !session_id();
-session_write_close();
+session_destroy();
 $trues[] = !session_id();
 
 Csrf::checkToken();
 $falses[] = !session_id();
-session_write_close();
+session_destroy();
 $trues[] = !session_id();
 
 Csrf::checkPostToken();
+$trues[] = !session_id();
+$_POST[Csrf::POST_KEY] = '-';
+Csrf::checkPostToken();
 $falses[] = !session_id();
-session_write_close();
+session_destroy();
 $trues[] = !session_id();
 
 Csrf::isValidToken();
 $falses[] = !session_id();
-session_write_close();
+session_destroy();
 $trues[] = !session_id();
 
 Csrf::isValidToken();
 $falses[] = !session_id();
-session_write_close();
+session_destroy();
 $trues[] = !session_id();
 
 $_sessionTestsResults = (object) array(
-	'trues' => $trues,
-	'falses' => $falses,
+    'trues' => $trues,
+    'falses' => $falses,
 );
 
 unset($trues);
 unset($falses);
 
-?>
